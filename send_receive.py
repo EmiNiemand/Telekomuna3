8f3261c8779d8b0465e9bfb90b9f2ae480340ad2
EmiNiemand <Szymon_PL@live.com>
2022-04-23 22:06:28 +0200

    minor changes

--- send_receive.py
+++ send_receive.py
@@ -3,13 +3,15 @@ import time
 
 
 def send(message):
+    # Receiver IP and port
+    server = ('127.0.0.1', 2137)
     # Create a client socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect to the server
-    client_socket.connect(("172.29.64.1", 2137))
+    client_socket.connect(("127.0.0.1", 2137))
     for byte in message:
-        time.sleep(0.05)
-        client_socket.send(byte)
+        time.sleep(0.01)
+        client_socket.sendto(byte, server)
 
 def receive():
     # Create a stream based socket(i.e, a TCP socket)
@@ -17,7 +19,7 @@ def receive():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind and listen
-    server_socket.bind(("172.29.64.1", 2137))
+    server_socket.bind(("127.0.0.1", 2137))
     server_socket.listen()
     data_from_client = []
     (client_connected, client_address) = server_socket.accept()
