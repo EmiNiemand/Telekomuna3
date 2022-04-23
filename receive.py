078fa2bb4d5f36cd83f92fef7760c1d8f04e9ff0
EmiNiemand <Szymon_PL@live.com>
2022-04-23 21:33:52 +0200

    update

--- receive.py
+++ receive.py
@@ -1,13 +1,16 @@
 import encode_decode_message as edm
 import send_receive as sr
+import codecs
 
 def main():
     dictionary = edm.create_dictionary()
     encoded_message = sr.receive()
-    print(encoded_message)
     decoded_message = edm.decode_message(encoded_message, dictionary)
     print(decoded_message)
-
+    f = codecs.open('received_message.txt', 'w+', 'utf-8')
+    f.write(decoded_message)
+    f.close()
 
 if __name__ == '__main__':
     main()
+
--- send_receive.py
+++ send_receive.py
@@ -5,12 +5,10 @@ import time
 def send(message):
     # Create a client socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(message)
     # Connect to the server
-    client_socket.connect(("127.0.0.1", 2137))
+    client_socket.connect(("172.29.64.1", 2137))
     for byte in message:
         time.sleep(0.05)
-        print(byte)
         client_socket.send(byte)
 
 def receive():
@@ -19,14 +17,13 @@ def receive():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind and listen
-    server_socket.bind(("127.0.0.1", 2137))
+    server_socket.bind(("172.29.64.1", 2137))
     server_socket.listen()
     data_from_client = []
     (client_connected, client_address) = server_socket.accept()
     # Accept connections
     while True:
         data = client_connected.recv(16)
-        print(data)
         if data:
             data_from_client.append(data)
         else:
