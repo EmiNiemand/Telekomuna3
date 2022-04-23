554a4823a75aaf48da040c45d64d8332af5066c6
EmiNiemand <Szymon_PL@live.com>
2022-04-23 21:37:05 +0200

    im finished for now

--- receive.py
+++ receive.py
@@ -6,7 +6,6 @@ def main():
     dictionary = edm.create_dictionary()
     encoded_message = sr.receive()
     decoded_message = edm.decode_message(encoded_message, dictionary)
-    print(decoded_message)
     f = codecs.open('received_message.txt', 'w+', 'utf-8')
     f.write(decoded_message)
     f.close()
